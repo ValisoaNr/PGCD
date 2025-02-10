3942f35ee22fff7bb2c5f4e4cec08d0d5a553f9e
ValisoaNr <valisoandr12@gmail.com>
2025-02-10 14:49:49 +0300

    Add files via upload

---
+++ code18.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main ()
+{
+	int a , b , r ;
+	
+	printf("\tCALCULE DU pgcd DE a ET b DIFFERENT DE 0\nEntrez la valeur de a :");//Presentation du programme
+	scanf("%d", &a);
+	printf("Entrez la valeur de b :");
+	scanf("%d", &b);
+	
+	if( (a%b) == 0 )//pour a divisible par b
+	{
+      		printf("pgcd (a,b) = %d\n", b );
+	}
+		else
+		{
+			do
+			{
+		    		r = a % b ;
+				a = b ;
+				b = r ;
+				r = ( a % b );	
+			}while( r != 0 ) ;
+			printf("pgcd (a,b) = %d\n" , b); //affichage de b , le dernier reste non nulle 
+		}
+	return (0);
+}
